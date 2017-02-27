02ce13adba4924ae3a9d9d10ab23c2557d2efbe5
kentsay <naturen@gmail.com>
2017-02-27 15:22:54 +0100

    add mongodb to store user information

--- chatapi/app/api/rcvBibleBot.py
+++ chatapi/app/api/rcvBibleBot.py
@@ -3,6 +3,7 @@
 import string
 import json
 import requests
+from pymongo import MongoClient
 
 from app import log
 from app.database import redis_db
@@ -24,26 +25,43 @@ def chatBotResponse(message):
     fbid = fbUtil.getFbId(message)
     recevied_message = fbUtil.getFbMessage(message)
 
-    checkFbUser(fbid)
-    query = recevied_message.split(" ")
-    book = bible_re.get_book(query[0])
-    if  book is not None:
-        doc_index = book[1] + query[1]
-        if len(r.smembers(doc_index)) == 0:
-            result = "Oops, cannot find this verse in the Bible."
-        else:
-            ## check if it's a single verse or a range
-            result = recevied_message + " " + next(iter(r.smembers(doc_index)))
+    if checkFbUser(fbid):
+        result = "Hi, welcome to use Recovery version Bible Chat Bot!"
     else:
-        result = "Oops, cannot find this verse in the Bible."
+        query = recevied_message.split(" ")
+        book = bible_re.get_book(query[0])
+        if  book is not None:
+            doc_index = book[1] + query[1]
+            if len(r.smembers(doc_index)) == 0:
+                result = "Oops, cannot find this verse in the Bible."
+            else:
+                ## check if it's a single verse or a range
+                result = recevied_message + " " + next(iter(r.smembers(doc_index)))
+        else:
+            result = "Oops, cannot find this verse in the Bible."
 
     responseFbMessage(fbid, result)
 
 def checkFbUser(fbid):
-    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
-    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':config.FB_ACCESS_TOKEN}
-    user_details = requests.get(user_details_url, user_details_params).json()
-    print(user_details)
+    client = MongoClient()
+    db = client.rcvbot
+    results = db.rcvbot.find({"fbid": fbid})
+    if len(results) is 0:
+        # It's the first time for this user chat with the bot
+        user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
+        user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':config.FB_ACCESS_TOKEN}
+        user_details = requests.get(user_details_url, user_details_params).json()
+        print(user_details)
+        db.rcvbot.insert({
+                        "fbid": fbid,
+                        "first_name": user_details['first_name'],
+                        "last_name": user_details['last_name'],
+                        "profile_pic": user_details['profile_pic']
+                        })
+        return True
+    else:
+        # Not a fish
+        return False
 
 def responseFbMessage(fbid, message):
     post_message_url = config.FB_MESSAGE_URL + config.FB_ACCESS_TOKEN
